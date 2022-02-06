78aa911959e0a6c61d9588351db7c733b94bd3d4
Srinivasan Mohana Arunachalam <s.mohanaarunachalam@reply.de>
2022-02-06 09:59:38 +0100

    Fix kubebench wrapper

--- tools/kubebench.sh
+++ tools/kubebench.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
-kubectl delete jobs kube-bench
+echo "Deleting any existing kubebench job"
+kubectl delete jobs kube-bench || echo "No kube-bench job found"
+
+echo "Creating new kube-bench job"
 kubectl apply -f https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job.yaml
+
+PodName=$(kubectl get pods -o name -l app=kube-bench)
+Status=$(kubectl get ${PodName} -o json | jq -r ".status.phase")
+while [ "${Status}" != "Succeeded" ]
+do
+echo "Waiting for ${PodName} to complete"
+sleep 1
+Status=$(kubectl get ${PodName} -o json | jq -r ".status.phase")
+done
+
+echo "Trailing logs from ${PodName}"
+kubectl logs ${PodName}
