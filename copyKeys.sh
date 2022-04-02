a5a5b5f4bd2624e5ecf4c07a71ed99965b76805b
Srinivasan Mohana Arunachalam <srinivasan.ma2009@hotmail.com>
2022-04-02 06:51:43 -0400

    Comment copyKeys.sh file

--- copyKeys.sh
+++ copyKeys.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Requirements
+# 1. Ansible Vault 
+# 2. Sshpass
+
+# Create SSH key if it doesnt exist
 if [ ! -f ~/.ssh/id_rsa.pub ]
 then
 	ssh-keygen -t rsa -b 4096 -C "ansible@control.sysctls.com" -P "" -f ~/.ssh/id_rsa
@@ -8,13 +13,16 @@ fi
 # Kill previous ssh-agent processes
 pkill ssh-agent
 
-# Start a new ssh-agent process and add identities
+# Start a new ssh-agent process and add default identity
 eval $(ssh-agent)
 ssh-add ~/.ssh/id_rsa
 
+# This step requires a text file ".vault" containig the vault password
+# and a Ansible Vault encrypted ".password" file containing the SSH password
 export SSHPASS=$(ansible-vault view .password --vault-password-file .vault)
 
-for address in $(seq 2 5)
+for server_number in $(seq 1 4)
 do 
-	sshpass -e ssh-copy-id -o StrictHostKeyChecking=no root@10.20.30.$address
+	# sshpass -e retrieves the SSH password from the SSHPASS environment variable
+	sshpass -e ssh-copy-id -o StrictHostKeyChecking=no root@server${server_number}
 done
